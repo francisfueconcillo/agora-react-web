c3200026f6c66b25dc87db9818c8e88b08e037f3
Francis Fueconcillo <francisfueconcillo@gmail.com>
2021-03-08 01:27:17 +0900

    Update api endpoint to use remote api in gcp

--- src/agora.config.js
+++ src/agora.config.js
@@ -1,2 +1,2 @@
 export const AGORA_APP_ID = 'ba22c24da19b40c8a85f3a6d84709499' // set your app id here
-export const SERVER_API_URL = 'http://localhost:9000'
+export const SERVER_API_URL = 'https://agora-api-dot-agora-react.et.r.appspot.com'
--- src/components/AgoraVideoCall/api.js
+++ src/components/AgoraVideoCall/api.js
@@ -3,21 +3,21 @@ import { SERVER_API_URL } from '../../agora.config.js';
 
 
 export const generateToken = async(cname, uid, attendee) => {
-  const url = `${SERVER_API_URL}/token?cname=${cname}&uid=${uid}&attendee=${attendee}`;
+  const url = `${SERVER_API_URL}/api/token?cname=${cname}&uid=${uid}&attendee=${attendee}`;
   const response = await axios.get(url);
   return response.data;
 
 }
 
 export const startRecording = async (cname, uid, ctype, token) => {
-  const url = `${SERVER_API_URL}/start?cname=${cname}&uid=${uid}&ctype=${ctype}&token=${token}`;
+  const url = `${SERVER_API_URL}/api/start?cname=${cname}&uid=${uid}&ctype=${ctype}&token=${token}`;
   const response = await axios.get(url);
   return response;
 }
 
 
 export const stopRecording = async (cname, uid, resid, sid) => {
-  const url = `${SERVER_API_URL}/stop?cname=${cname}&uid=${uid}&resid=${resid}&sid=${sid}`;
+  const url = `${SERVER_API_URL}/api/stop?cname=${cname}&uid=${uid}&resid=${resid}&sid=${sid}`;
   const response = await axios.get(url);
   return response; 
 }
